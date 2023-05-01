cf6385fe1909c6d46faab5483f8438c85e948878
Akash Yadav <akash.yadav@bolt.eu>
2023-05-01 16:18:04 +0300

    implementing haskey logic

--- cache-cli/pkg/storage/local_storage_has_key.go
+++ cache-cli/pkg/storage/local_storage_has_key.go
@@ -1,6 +1,13 @@
 package storage
 
-func (s *LocalStorage) HasKey(key string) (bool, error) {
+import (
+	"os"
+)
 
+func (s *LocalStorage) HasKey(key string) (bool, error) {
+	_, err := os.Stat(key)
+	if os.IsNotExist(err) || err != nil {
+		return false, err
+	}
 	return true, nil
 }
