3a73d6b505cb5868418ce5a9d38123133c498973
Akash Yadav <akash.yadav@bolt.eu>
2023-05-01 18:06:09 +0300

    implementing re-store logic

--- cache-cli/pkg/storage/local_storage_restore.go
+++ cache-cli/pkg/storage/local_storage_restore.go
@@ -1,9 +1,36 @@
 package storage
 
 import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"io"
+	"io/ioutil"
 	"os"
+	"path"
 )
 
 func (s *LocalStorage) Restore(key string) (*os.File, error) {
-	return nil, nil
+
+	log.Infof("Restore Key %s :", key)
+
+	sourceFilePath := fmt.Sprintf("%s/%s", s.Path, path.Base(key))
+	sourceFile, err := os.Open(sourceFilePath)
+	if err != nil {
+		return nil, err
+	}
+	defer sourceFile.Close()
+
+	// Creating file in /tmp folder
+	destinationFile, err := ioutil.TempFile(os.TempDir(), fmt.Sprintf("%s-*", key))
+	if err != nil {
+		return nil, err
+	}
+	defer destinationFile.Close()
+
+	_, err = io.Copy(destinationFile, sourceFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return destinationFile, nil
 }
